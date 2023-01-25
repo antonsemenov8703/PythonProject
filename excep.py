00372b17182194eb0e0a70ed09d412972286155e
kolosova-anna <golikovaanna7365@mail.ru>
2023-01-25 16:19:17 +0200

    Add exceptions for complex numbers

--- excep.py
+++ excep.py
@@ -85,3 +85,29 @@ def is_number_all(x, y):
             return False
 
 
+# Проверка комплексных чисел:
+# 1. Нельзя использовать //, %
+# 2. Нельзя пользоваться функцией sqrt
+
+
+# Проверка ввода комплексного числа
+
+def check_complex1 ():
+    n = input ('Enter real part: ')
+    while not n.isdigit():
+        print ('Error')
+        n = input ('Enter once again: ')
+    return int (n)
+
+def check_complex2 ():
+    n = input ('Enter imaginary number: ')
+    while not n.isdigit():
+        print ('Error')
+        n = input ('Enter once again: ')
+    return int (n)
+
+# Ввод каждой части комплексного числа осуществляется следующим образом:
+# a = check_complex1()
+# b = check_complex2()
+# num = complex (a, b)
+# print (num)
